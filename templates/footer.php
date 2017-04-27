5463af1448078f8549d37663e946ee9d9c965b80
Peter Bateman <peter_bateman@hotmail.co.uk>
2017-04-27 23:58:28 +0100

    front page mobile nearly there

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,11 @@
-<footer class="content-info">
+<footer class="content-info footer">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <div class="row text-center">
+      <p class="footer-address">Cassington, Whitney, Oxfordshire, OX29</p>
+    </div>
+    <div class="row text-center">
+      <p class="footer-copyright">&copy <?php echo date("Y")?> I Do Vintage</p>
+    </div>
   </div>
 </footer>
--- templates/front-page.php
+++ templates/front-page.php
@@ -8,18 +8,67 @@
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
 
-<div class="hero">
-  <div class="container">
-    <div class="row">
-      <p>hello</p>
-
-    </div>
-
+<div class="row hero">
+  <img src="http://localhost/idovintage/wp-content/uploads/2017/04/4.jpg" alt="tea cup">
+  <div class="hero-text">
+    <p>We have a wonderful range</p>
+    <p>of vintage china. All lovingly</p>
+    <p>refound and reused.</p>
+    <button type="button" class="btn btn-collection text-uppercase">Our collection</button>
   </div>
 </div>
 
-<div class="jumbotron">
-  <h1>Hello, world!</h1>
-  <p>...</p>
-  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+<div class="row row-padding text-center cups">
+  <p >A few of our favourite cups...</p>
+  <div class="flexbox-cups">
+    <img src="http://localhost/idovintage/wp-content/uploads/2017/04/cup1.jpg" alt="">
+    <img src="http://localhost/idovintage/wp-content/uploads/2017/04/cup-3.jpg" alt="">
+    <img src="http://localhost/idovintage/wp-content/uploads/2017/04/cup-2.jpg" alt="">
+  </div>
+</div>
+
+<div class="row row-padding text-center info-columns">
+  <div class="col-sm-4 col-md-4">
+    <div class="thumbnail">
+      <img src="http://localhost/idovintage/wp-content/uploads/2017/04/price.jpg" alt="">
+    </div>
+    <div class="caption">
+      <h2 class="caption-header">Pricing</h2>
+      <p class="caption-text">We have a variety of packages on offer. There  are all sorts of different things you can get. There is a  minimum orderof £40. If you wish to do such and such you must accept this policy or wewill not give you our lovely china, sothere, okay! </p>
+    </div>
+  </div>
+  <div class="col-sm-4 col-md-4">
+    <div class="thumbnail">
+      <img src="http://localhost/idovintage/wp-content/uploads/2017/04/delivery.jpg" alt="">
+    </div>
+    <div class="caption">
+      <h2 class="caption-header">Delivery</h2>
+      <p class="caption-text">We have a variety of packages on offer. There  are all sorts of different things you can get. There is a  minimum orderof £40. If you wish to do such and such you must accept this policy or wewill not give you our lovely china, sothere, okay! </p>
+    </div>
+  </div>
+  <div class="col-sm-4 col-md-4">
+    <div class="thumbnail">
+      <img src="http://localhost/idovintage/wp-content/uploads/2017/04/Contact.jpg" alt="">
+    </div>
+    <div class="caption">
+      <h2>Contact</h2>
+      <p>We have a variety of packages on offer. There  are all sorts of different things you can get. There is a  minimum orderof £40. If you wish to do such and such you must accept this policy or wewill not give you our lovely china, sothere, okay! </p>
+    </div>
+  </div>
+</div>
+
+<div class="row my-row">
+  <div class="another">
+    <div class="testamonial-background">
+      <div class="testamonial text-center">
+          <div class="testamonial-text">
+            <p>"Everybody loved the tea with vintage china. The service was very proffessional and
+        we will definitely use them again"</p>
+            <p class="testamonial-name">Sarah Lorrie</p>
+          </div>
+        </div>
+    </div>
+  </div>
+
+
 </div>
